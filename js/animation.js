163e465c358b3639278092e19ca84d05a66673d6
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-10-05 23:23:43 -0400

    Work so far

--- js/animation.js
+++ js/animation.js
@@ -3,11 +3,19 @@ import { TrackballControls } from './lib/TrackballControls.js';
 import { DynamicsController } from './dynamics.js';
 
 var scene, camera, renderer, controls;
-var sunlight, sun, earth, spacecraft;
+var sunlight, sun, earth, leader, follower;
+
+var rArrow, vArrow, sArrow;
+var xArrow, yArrow, zArrow;
+
 var dynamics;
 var t = 0;
-const dt = 0.1; // dt corresponds to a minute of simulated time,
-                // and to a frame of real time ~ 1/60 seconds.
+const dt_animation = 0.1; // dt corresponds to a minute of simulated time,
+                          // and to a frame of real time ~ 1/60 seconds.
+                          // This is a reasonable frame rate that makes the
+                          // animation fast but not too fast.
+const dt_realtime = 1 / 3600; // This will produce a realtime simulation.
+const dt = dt_animation;
 
 const sunRadius = 30;
 const earthRadius = 63.7;
@@ -23,7 +31,7 @@ function init() {
     scene.add(new THREE.AmbientLight(0x333333));
 
     // Sun and sunlight
-    sunlight = new THREE.PointLight(0xffffff, 1.25);
+    sunlight = new THREE.DirectionalLight(0xffffff, 1);
     scene.add(sunlight);
     const sunMaterial = new THREE.MeshPhongMaterial({
         map: THREE.ImageUtils.loadTexture('images/sunmap.jpg'),
@@ -44,28 +52,79 @@ function init() {
     earth.scale.set(earthRadius, earthRadius, earthRadius);
     scene.add(earth);
 
-    // Spacecraft
-    var greyMetalMaterial = new THREE.MeshBasicMaterial({ color: 0x999896 });
-    spacecraft = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 2), greyMetalMaterial);
-    scene.add(spacecraft);
+    // Spacecrafts
+    var leaderSpacecraftMaterial = new THREE.MeshBasicMaterial({ color: 0xb5b535 });
+    var followerSpacecraftMaterial = new THREE.MeshBasicMaterial({ color: 0xa241cc });
+    leader = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 2), leaderSpacecraftMaterial);
+    follower = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 2), followerSpacecraftMaterial);
+    scene.add(leader);
+    scene.add(follower);
 
-    dynamics = new DynamicsController(sun, sunlight, earth, spacecraft);
-    dynamics.setMode("earth");
-    dynamics.initialize_positions(camera);
-}
+    dynamics = new DynamicsController(sun, earth, leader, follower);
+    dynamics.setMode("leader", camera);
 
-function update_spacecraft_centered(earth, sun, sunlight, spacecraft) {
+    rArrow = new THREE.ArrowHelper(new THREE.Vector3(), new THREE.Vector3(), 2, 0xffffff);
+    vArrow = new THREE.ArrowHelper(new THREE.Vector3(), new THREE.Vector3(), 2, 0x707070);
+    sArrow = new THREE.ArrowHelper(new THREE.Vector3(), new THREE.Vector3(), 2, 0xffff00);
+    xArrow = new THREE.ArrowHelper(new THREE.Vector3(), new THREE.Vector3(), 2, 0xff0000);
+    yArrow = new THREE.ArrowHelper(new THREE.Vector3(), new THREE.Vector3(), 2, 0x00ff00);
+    zArrow = new THREE.ArrowHelper(new THREE.Vector3(), new THREE.Vector3(), 2, 0x0000ff);
+    scene.add(rArrow);
+    scene.add(vArrow);
+    scene.add(sArrow);
+    scene.add(xArrow);
+    scene.add(yArrow);
+    scene.add(zArrow);
 
-}
+    var loader = new THREE.FontLoader();
 
-function update_earth_centered(earth, sun, sunlight, spacecraft) {
-    
+    loader.load('fonts/helvetiker_regular.typeface.json', function (font) {
+        var geometry = new THREE.TextGeometry('Hello three.js!', {
+            font: font,
+            size: 80,
+            height: 5,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+        });
+    });
 }
 
 var animate = function () {
     requestAnimationFrame(animate);
 
     dynamics.update(t, dt);
+    sunlight.position.x = sun.position.x;
+    sunlight.position.y = sun.position.y;
+    sunlight.position.z = sun.position.z;
+    rArrow.setDirection(new THREE.Vector3().copy(dynamics.leaderPosition).normalize());
+    vArrow.setDirection(new THREE.Vector3().copy(dynamics.leaderVelocity).normalize());
+    sArrow.setDirection(new THREE.Vector3().copy(sun.position).normalize());
+    xArrow.setDirection(new THREE.Vector3(1,0,0));
+    yArrow.setDirection(new THREE.Vector3(0,1,0));
+    zArrow.setDirection(new THREE.Vector3(0,0,1));
+    rArrow.position.x = dynamics.leaderPosition.x;
+    rArrow.position.y = dynamics.leaderPosition.y;
+    rArrow.position.z = dynamics.leaderPosition.z;
+    vArrow.position.x = dynamics.leaderPosition.x;
+    vArrow.position.y = dynamics.leaderPosition.y;
+    vArrow.position.z = dynamics.leaderPosition.z;
+    sArrow.position.x = dynamics.leaderPosition.x;
+    sArrow.position.y = dynamics.leaderPosition.y;
+    sArrow.position.z = dynamics.leaderPosition.z;
+    xArrow.position.x = dynamics.leaderPosition.x;
+    xArrow.position.y = dynamics.leaderPosition.y;
+    xArrow.position.z = dynamics.leaderPosition.z;
+    yArrow.position.x = dynamics.leaderPosition.x;
+    yArrow.position.y = dynamics.leaderPosition.y;
+    yArrow.position.z = dynamics.leaderPosition.z;
+    zArrow.position.x = dynamics.leaderPosition.x;
+    zArrow.position.y = dynamics.leaderPosition.y;
+    zArrow.position.z = dynamics.leaderPosition.z;
+
     controls.update();
 
     t += dt;
@@ -75,3 +134,19 @@ var animate = function () {
 
 init();
 animate();
+
+var buttons = document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", onButtonClick, false);
+};
+
+function onButtonClick(event) {
+    if (event.target.id == "leader" || event.target.id == "follower")
+    {
+        dynamics.setMode("leader", camera);
+    }
+    else if (event.target.id == "earth")
+    {
+        dynamics.setMode("earth", camera);
+    }
+}
--- js/dynamics.js
+++ js/dynamics.js
@@ -1,9 +1,9 @@
 import * as THREE from './lib/three.module.js';
 
-const R_sun = 500;
+const R_sun = 1000;
 const R_orbit = 68.7;
 
-const T_orbit = 90; // Corresponds to 90 minutes
+const T_orbit = 94; // Corresponds to 94 minutes, the period of a spacecraft orbit
 const T_earth_revolution = 60 * 24; // Corresponds to a day
 const T_sun_orbit = 60 * 24 * 365; // Corresponds to a year
 
@@ -13,34 +13,12 @@ const T_earth_revolution_normalized = T_earth_revolution / (2 * Math.PI);
 const T_sun_orbit_normalized = T_sun_orbit / (2 * Math.PI);
 
 class DynamicsController {
-    constructor(sun, sunlight, earth, spacecraft)
+    constructor(sun, earth, leader, follower)
     {
         this.sun = sun;
-        this.sunlight = sunlight;
         this.earth = earth;
-        this.spacecraft = spacecraft;
-    }
-
-    setMode(mode)
-    {
-        this.mode = mode;
-    }
-
-    initialize_positions(camera)
-    {
-        if (this.mode == "spacecraft")
-        {
-            this.earth.position.set(-R_orbit, 0, 0);
-            this.sunlight.position.z = this.sun.position.z;
-            this.spacecraft.position.set(0, 0, 0);
-            camera.position.x = 50;
-        }
-        else if (this.mode == "earth") {
-            this.earth.position.set(0, 0, 0);
-            this.sunlight.position.z = this.sun.position.z;
-            this.spacecraft.position.set(R_orbit, 0, 0);
-            camera.position.z = 200;
-        }
+        this.leader = leader;
+        this.follower = follower;
 
         this.sunInclinationMatrix = new THREE.Matrix3();
         const sunInclination = (23.5 / 360) * 2 * Math.PI;
@@ -48,55 +26,92 @@ class DynamicsController {
             0, Math.cos(sunInclination), Math.sin(sunInclination),
             0, -Math.sin(sunInclination), Math.cos(sunInclination));
 
-        this.spacecraftInclinationMatrix = new THREE.Matrix3();
+        this.leaderInclinationMatrix = new THREE.Matrix3();
         const orbitInclination = (45 / 360) * 2 * Math.PI;
-        this.spacecraftInclinationMatrix.set(1, 0, 0,
+        this.leaderInclinationMatrix.set(1, 0, 0,
             0, Math.cos(orbitInclination), Math.sin(orbitInclination),
             0, -Math.sin(orbitInclination), Math.cos(orbitInclination));
+    }
+
+    setMode(mode, camera)
+    {
+        this.mode = mode;
+        if (this.mode == "leader" || this.mode == "follower") {
+            this.earth.position.set(-R_orbit, 0, 0);
+            this.leader.position.set(0, 0, 0);
+            this.follower.position.set(0, 0, 0);
+            camera.position.x = 50;
+        }
+        else if (this.mode == "earth") {
+            this.earth.position.set(0, 0, 0);
+            this.leader.position.set(R_orbit, 0, 0);
+            this.follower.position.set(R_orbit, 0, 0);
+            camera.position.z = 200;
+        }
     }
 
     /**
      * Compute the real positions of objects in ECEF.
      */
-    updateRealPhysics(t, dt) {
-        var planarSunOrbit = new THREE.Vector3(R_sun * Math.sin(t / T_sun_orbit_normalized), 0, R_sun * Math.cos(t / T_sun_orbit_normalized));
-        this.sunOrbit = planarSunOrbit.applyMatrix3(this.sunInclinationMatrix);
+    updatePhysics(t, dt) {
+        var planarSunPosition = new THREE.Vector3(
+            R_sun * Math.sin(t / T_sun_orbit_normalized), 0, R_sun * Math.cos(t / T_sun_orbit_normalized));
+        this.sunPosition = planarSunPosition.applyMatrix3(this.sunInclinationMatrix);
         
-        var planarSpacecraftOrbit = new THREE.Vector3(R_orbit * Math.sin(t / T_orbit_normalized), 0, R_orbit * Math.cos(t / T_orbit_normalized));
-        this.spacecraftOrbit = planarSpacecraftOrbit.applyMatrix3(this.spacecraftInclinationMatrix);
+        var planarLeaderPosition = new THREE.Vector3(
+            R_orbit * Math.sin(t / T_orbit_normalized), 0, R_orbit * Math.cos(t / T_orbit_normalized));
+        var planarLeaderVelocity = new THREE.Vector3(
+            R_orbit * Math.cos(t / T_orbit_normalized), 0, - R_orbit * Math.sin(t / T_orbit_normalized));
+        this.leaderPosition = planarLeaderPosition.applyMatrix3(this.leaderInclinationMatrix);
+        this.leaderVelocity = planarLeaderVelocity.applyMatrix3(this.leaderInclinationMatrix);
 
         this.earthRotation = new THREE.Vector3(0, t / T_earth_revolution_normalized, 0);
     }
 
     update(t, dt)
     {
-        this.updateRealPhysics(t, dt);
-        if (this.mode == "earth") this.updateEarth();
-        if (this.mode == "spacecraft") this.updateSpacecraft();
+        this.updatePhysics(t, dt);
+        if (this.mode == "earth") this.updateEarth(t, dt);
+        else if (this.mode == "leader") this.updateLeader(t, dt);
     }
 
-    updateEarth()
+    updateEarth(t, dt)
     {
-        this.sun.position.x = this.sunOrbit.x;
-        this.sun.position.y = this.sunOrbit.y;
-        this.sun.position.z = this.sunOrbit.z;
-        this.sunlight.position.x = this.sunOrbit.x;
-        this.sunlight.position.y = this.sunOrbit.y;
-        this.sunlight.position.z = this.sunOrbit.z;
-        this.spacecraft.position.x = this.spacecraftOrbit.x;
-        this.spacecraft.position.y = this.spacecraftOrbit.y;
-        this.spacecraft.position.z = this.spacecraftOrbit.z;
+        this.sun.position.x = this.sunPosition.x;
+        this.sun.position.y = this.sunPosition.y;
+        this.sun.position.z = this.sunPosition.z;
+        this.leader.position.x = this.leaderPosition.x;
+        this.leader.position.y = this.leaderPosition.y;
+        this.leader.position.z = this.leaderPosition.z;
 
         this.earth.rotation.x = this.earthRotation.x;
         this.earth.rotation.y = this.earthRotation.y;
         this.earth.rotation.z = this.earthRotation.z;
-
-        console.log(this.spacecraft.position);
     }
 
-    updateSpacecraft()
+    updateLeader(t, dt)
     {
+        // Compute ECEF-to-leader transform
+        var r = this.leaderPosition.normalize();
+        var v = this.leaderVelocity.normalize();
+        var w = v.cross(r).normalize();
+        var T_ecef_hill = new THREE.Matrix3(); // Transformation from frame defined by r, v, r x v to ECEF
+        T_ecef_hill.set(r.x, w.x, v.x,
+                        r.y, w.y, v.y,
+                        r.z, w.z, v.z);
+        var T_hill_ecef = T_ecef_hill.transpose();
 
+        var sunPosition = this.sunPosition.sub(this.leaderPosition);
+        sunPosition = sunPosition.applyMatrix3(T_hill_ecef);
+
+        this.sun.position.x = sunPosition.x;
+        this.sun.position.y = sunPosition.y;
+        this.sun.position.z = sunPosition.z;
+
+        var earthRotation = new THREE.Vector3(0, t / T_earth_revolution_normalized, 0);
+        this.earth.rotation.x = earthRotation.x;
+        this.earth.rotation.y = earthRotation.y;
+        this.earth.rotation.z = earthRotation.z;
     }
 }
 
